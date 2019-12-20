33d317850b4018e64481d2f789d99f751c8c3fec
20641118 <callehj@hotmail.se>
2019-12-20 13:13:57 +0100

    Change theme colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,8 @@ module.exports = {
 				name: "SpotiFest",
 				short_name: "SpotiFest",
 				start_url: "/",
-				background_color: "#663399",
-				theme_color: "#663399",
+				background_color: "#0B1D2E",
+				theme_color: "#193C5E",
 				display: "minimal-ui",
 				icon: "src/images/logo/icon.png" // This path is relative to the root of the site.
 			}
