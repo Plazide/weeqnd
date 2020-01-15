8f2114ca02e20901067e5da3df952b1b38aa2e4c
20641118 <callehj@hotmail.se>
2020-01-15 10:32:17 +0100

    Try to handle redirects, but it doesn't work

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ const express = require("express");
 const mockEvent = require("../service/event.json");
 const mockContext = require("../service/context.json");
 const handler = require("../service/handler");
+const emulate = require("./emulate");
+const bodyParser = require("body-parser");
 
 const PORT = 8001;
 const app = express();
 
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(emulate());
 app.use("/api", async (req, res) => {
 	const event = { ...mockEvent, ...req };
 	const context = mockContext;
 
 	try{
-		const path = req.path.replace(/\/api/, "").replace(/\//, "");
+		const path = req.path.replace(/\/api/, "").replace(/\//, "").split("?")[0];
 		const func = handler[path];
 
 		if(typeof func !== "function")
@@ -19,8 +24,14 @@ app.use("/api", async (req, res) => {
 
 		const{ statusCode, body, headers } = await func(event, context);
 
-		res.set(headers).status(statusCode).json(body);
+		if(statusCode === 301 || statusCode === 302){
+			res.redirect(statusCode, headers.Location);
+			return;
+		}
+
+		res.status(statusCode).set(headers).json(body ? JSON.parse(body) : "");
 	}catch(err){
+		console.error(err);
 		res.status(500).json({ message: "Internal Server Error", statusCode: 500 });
 	}
 });
