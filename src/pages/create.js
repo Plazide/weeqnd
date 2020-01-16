a62e7388e1f85ca5c841c5e6fc22eb8e3246f601
20641118 <callehj@hotmail.se>
2020-01-16 16:46:42 +0100

    Add handler for submit

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { navigate } from "gatsby";
 import PropTypes from "prop-types";
 
 // Layout
@@ -20,6 +21,7 @@ import "../styles/create.css";
 const Create = () => {
 	const[loaded, setLoaded] = useState(false);
 	const[playlists, setPlaylists] = useState([]);
+	const[error, setError] = useState(null);
 
 	useEffect( () => {
 		async function auth (){
@@ -37,10 +39,12 @@ const Create = () => {
 
 			if(response.ok){
 				const result = await response.json();
+				if(result.error)
+					navigate("/");
 
-				setLoaded(true);
 				spotify.setAccessToken(result.access_token);
 				spotify.setRefreshToken(result.refresh_token);
+				setLoaded(true);
 			}
 		}
 
@@ -48,8 +52,8 @@ const Create = () => {
 			const result = await spotify.getUsersPlaylists();
 			const items = result.items;
 
-			console.log(items);
-			setPlaylists(items);
+			if(items)
+				setPlaylists(items);
 		}
 
 		if(!loaded)
@@ -58,6 +62,35 @@ const Create = () => {
 		if(loaded && playlists.length === 0)
 			getPlaylists();
 	});
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+
+		const form = e.target;
+		const endpoint = form.action;
+		const playlist = form.querySelector("[name=\"playlist\"]:checked");
+
+		if(!playlist)
+			return setError("Välj en spellista!");
+
+		const options = {
+			method: "POST",
+			body: JSON.stringify({
+				playlist: playlist.value,
+				accessToken: spotify.accessToken,
+				refreshToken: spotify.refreshToken
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+
+		const response = await fetch(endpoint, options);
+
+		if(response.ok){
+			// Redirect to Party Page
+		}
+	};
 
 	return(
 		<Layout>
@@ -71,21 +104,25 @@ const Create = () => {
 					<div className="select-playlist">
 						<h2>Nödlista <Info>Listan som kommer att spelas om kön är tom.</Info></h2>
 
-						<div className="songs">
-							{playlists.map( (playlist) => {
-								return(
-									<Playlist
-										name={playlist.name}
-										total={playlist.tracks.total}
-										href={playlist.href}
-										key={playlist.name}
-									/>
-								);
-							})}
-						</div>
+						<form action="/api/create" onSubmit={onSubmit}>
+							<div className="songs">
+								{playlists ? playlists.map( (playlist) => {
+									return(
+										<Playlist
+											name={playlist.name}
+											total={playlist.tracks.total}
+											href={playlist.href}
+											key={playlist.name}
+										/>
+									);
+								}) : "loading...."}
+							</div>
+							<div className={`error ${error ? "show" : ""}`} role="alert">{error}</div>
+							<Button variant="filled">Skapa</Button>
+						</form>
+
 					</div>
 
-					<Button variant="filled">Skapa</Button>
 				</div>
 			</section>
 		</Layout>
