cfbd4268001978f033e71fb302d2b7e39c4c6f61
Carl Hallén Jansson <callehj@hotmail.se>
2020-02-23 15:46:46 +0100

    Add try/catch block

--- service/spotify/refresh.js
+++ service/spotify/refresh.js
@@ -5,42 +5,44 @@ require("dotenv").config();
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
-async function refresh (event){
-	const body = event.body;
-	const refreshToken = body.refreshToken;
+async function refresh(event){
+	try{
+		const body = event.body;
+		const refreshToken = body.refreshToken;
 
-	const spotifyApi = new SpotifyWebApi({
-		clientId,
-		clientSecret,
-		refreshToken
-	});
+		const spotifyApi = new SpotifyWebApi({
+			clientId,
+			clientSecret,
+			refreshToken
+		});
 
-	const result = await spotifyApi.refreshAccessToken();
-	const newAccessToken = result.body.accessToken;
-	const newRefreshToken = result.body.refreshToken;
+		const result = await spotifyApi.refreshAccessToken();
+		const newAccessToken = result.body.accessToken;
+		const newRefreshToken = result.body.refreshToken;
 
-	spotifyApi.setAccessToken(newAccessToken);
-	const me = await spotifyApi.getMe();
-	const username = me.body.id;
-	const data = { accessToken: newAccessToken };
+		spotifyApi.setAccessToken(newAccessToken);
+		const me = await spotifyApi.getMe();
+		const username = me.body.id;
+		const data = { accessToken: newAccessToken };
 
-	// Only set the refresh token if it exists. It is not returned with every refresh.
-	if(newRefreshToken) data.refreshToken = newRefreshToken;
+		// Only set the refresh token if it exists. It is not returned with every refresh.
+		if(newRefreshToken) data.refreshToken = newRefreshToken;
 
-	const updated = await db({
-		type: "mutation",
-		name: "partialUpdateParty",
-		data,
-		args: { owner: username },
-		returns: ["accessToken"]
-	});
+		const updated = await db({
+			type: "mutation",
+			name: "partialUpdateParty",
+			data,
+			args: { owner: username },
+			returns: ["accessToken"]
+		});
 
-	console.log(updated);
-
-	return{
-		statusCode: result.statusCode,
-		body: JSON.stringify(result.body)
-	};
+		return{
+			statusCode: result.statusCode,
+			body: JSON.stringify(result.body)
+		};
+	}catch(err){
+		throw err;
+	}
 }
 
 module.exports = refresh;
