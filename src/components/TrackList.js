73a29850ee7d63fa09345f0c022c9d5cc956f378
Carl Hallén Jansson <callehj@hotmail.se>
2020-02-25 15:59:54 +0100

    Forgot to stage this file

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 // Components
@@ -10,8 +10,11 @@ import "./styles/list.css";
 // Icons
 import PlaylistAdd from "../images/icons/playlist-add.svg";
 import PlaylistAdded from "../images/icons/playlist-added.svg";
+import MoreIcon from "../images/icons/more.svg";
+import DeleteIcon from "../images/icons/delete.svg";
+import SettingsIcon from "../images/icons/settings.svg";
 
-export default function TrackList({ tracks, playlist, adding, onClick }){
+export default function TrackList({ tracks, playlist, adding, onClick, activePlaylist = false }){
 	return(
 		<ul className="list">
 			{tracks.map( track =>
@@ -21,15 +24,16 @@ export default function TrackList({ tracks, playlist, adding, onClick }){
 					key={track.id}
 					onClick={onClick}
 					adding={adding}
+					activePlaylist={activePlaylist}
 				/>)}
 		</ul>
 	);
 }
 
-const Track = ({ track, playlist, onClick, adding }) => {
-	console.log(adding);
+const Track = ({ track, playlist, onClick, adding, activePlaylist }) => {
 	const{ name, artists, album, id } = track;
-	const inPlaylist = playlist.some( item => item.id === id);
+	const currentTrack = playlist.find( item => item.id === id);
+	const inPlaylist = currentTrack !== undefined;
 	const image = album.images.reduce((prev, curr) => {
 		if(curr.width < prev.width) return curr;
 	});
@@ -51,11 +55,28 @@ const Track = ({ track, playlist, onClick, adding }) => {
 
 			</div>
 			<div className="column add">
-				<Loader load={adding === id} className="track-add-load">
-					{!inPlaylist ? <PlaylistAdd onClick={ () => { onClick(id); }} /> : <PlaylistAdded />}
-				</Loader>
+				{activePlaylist ? <TrackOptions track={currentTrack} />
+					: <Loader load={adding === id} className="track-add-load">
+						{!inPlaylist ? <PlaylistAdd onClick={ () => { onClick(id); }} /> : <PlaylistAdded />}
+					</Loader>
+				}
+
 			</div>
 		</li>
+	);
+};
+
+const TrackOptions = ({ onRemove, track }) => {
+	const[open, setOpen] = useState(false);
+
+	return(
+		<div className="options">
+			<button className="options__buttonMore" onClick={ () => { setOpen(!open); } }><MoreIcon /></button>
+			<div className={`options__more ${open ? "options__more--open" : "options__more--closed"}`}>
+				<button onClick={ () => { onRemove(track); }} className="options__action"><DeleteIcon /></button>
+				<button className="options__action"><SettingsIcon /></button>
+			</div>
+		</div>
 	);
 };
 
@@ -64,12 +85,19 @@ TrackList.propTypes = {
 	title: PropTypes.string,
 	playlist: PropTypes.array,
 	adding: PropTypes.string,
-	onClick: PropTypes.func
+	onClick: PropTypes.func,
+	activePlaylist: PropTypes.bool
 };
 
 Track.propTypes = {
 	track: PropTypes.object.isRequired,
 	playlist: PropTypes.array,
 	adding: PropTypes.string,
-	onClick: PropTypes.func
+	onClick: PropTypes.func,
+	activePlaylist: PropTypes.bool
+};
+
+TrackOptions.propTypes = {
+	onRemove: PropTypes.func,
+	track: PropTypes.object
 };
