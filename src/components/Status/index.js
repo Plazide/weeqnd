24586ed8dd946dc29800fa32b550be90bd02321e
Carl Hallén Jansson <callehj@hotmail.se>
2020-03-05 17:10:24 +0100

    Fix success messages not showing

--- src/components/Status/index.js
+++ src/components/Status/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 // models
 import errors from "../../models/errors";
+import success from "../../models/success";
 
 // css
 import "./style.css";
@@ -11,6 +12,7 @@ export default function Status({ type, msgCode, onExpire }){
 	const[hidden, setHidden] = useState(true);
 	const delay = type === "error" ? 6000 : 4000;
 	const role = type === "error" ? "alert" : "status";
+	const messages = type === "error" ? errors : success;
 
 	useEffect( () => {
 		if(msgCode){
@@ -28,7 +30,7 @@ export default function Status({ type, msgCode, onExpire }){
 	return(
 		<div className={`page-status ${type} ${hidden ? "hidden" : ""}`} role={role}>
 			<div className="inner">
-				<div className="message">{errors[msgCode]}</div>
+				<div className="message">{messages[msgCode]}</div>
 			</div>
 		</div>
 	);
