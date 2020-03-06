9c92284cd4460a3133eebe333e6b6c5c3865a985
Carl Hallén Jansson <callehj@hotmail.se>
2020-03-06 11:22:51 +0100

    Add default value for children prop

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,7 +3,7 @@ import React from "react";
 // Css
 import "./style.css";
 
-const Loader = ({ load = false, children, className = "" }) => {
+const Loader = ({ load = false, children = "", className = "" }) => {
 	const loading = <div className={`loader ${className} ${load ? "load" : ""}`}></div>;
 
 	return load ? loading : children;
