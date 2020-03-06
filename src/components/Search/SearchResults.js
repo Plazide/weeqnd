7f9b4a78357eff97dbde107f80e53406f9953c1e
Carl Hallén Jansson <callehj@hotmail.se>
2020-03-06 11:22:17 +0100

    Add infinite scroll

--- src/components/Search/SearchResults.js
+++ src/components/Search/SearchResults.js
@@ -4,11 +4,12 @@ import PropTypes from "prop-types";
 // Components
 import TrackList from "../Playlist/TrackList";
 import TopTracks from "../Playlist/TopTracks";
+import Loader from "../Loader/";
 
 // Contexts
 import { PartyContext, MethodContext, LoadingContext } from "../../contexts";
 
-const SearchResults = ({ results, playlist }) => {
+const SearchResults = ({ results, playlist, loadingMore }) => {
 	const{ topTracks } = useContext(PartyContext);
 	const{ onAddTrack } = useContext(MethodContext);
 	const{ adding } = useContext(LoadingContext);
@@ -21,13 +22,15 @@ const SearchResults = ({ results, playlist }) => {
 	return(
 		<section className="search-results">
 			<TrackList tracks={results} playlist={playlist} onClick={onAddTrack} adding={adding} />
+			<Loader className="load-more-loader" load={loadingMore} />
 		</section>
 	);
 };
 
 SearchResults.propTypes = {
 	results: PropTypes.array.isRequired,
-	playlist: PropTypes.array.isRequired
+	playlist: PropTypes.array.isRequired,
+	loadingMore: PropTypes.bool
 };
 
 export default SearchResults;
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -20,11 +20,17 @@ import "./style.css";
 
 const Search = () => {
 	const party = useContext(PartyContext);
-	const[results, setResults] = useState([]);
-	const[prevValue, setPrevValue] = useState(null);
 	const playlist = party.playlist;
+	const limit = 25;
+	const[results, setResults] = useState([]);
+	const[currentValue, setCurrentValue] = useState(null);
+	const[prevValue, setPrevValue] = useState(null);
+	const[offset, setOffset] = useState(limit);
+	const[loadingMore, setLoadingMore] = useState(false);
 
 	const onSearch = async (value) => {
+		setCurrentValue(value);
+
 		if(value === prevValue) return;
 
 		if(!value){
@@ -35,17 +41,48 @@ const Search = () => {
 		const result = await spotify.search({
 			value,
 			type: "track",
-			limit: 10
+			limit,
+			offset: 0
 		}, false);
 
 		setPrevValue(value);
 		setResults(result.tracks.items);
+		setOffset(limit);
+	};
+
+	async function loadMore(){
+		if(!currentValue) return;
+		const newOffset = offset + limit;
+
+		const result = await spotify.search({
+			value: currentValue,
+			type: "track",
+			limit,
+			offset: newOffset
+		});
+
+		setOffset(newOffset);
+		setResults([...results, ...result.tracks.items]);
+	};
+
+	const onScroll = async e => {
+		const container = e.target;
+		const scrollHeight = container.scrollHeight;
+		const height = container.clientHeight;
+		const scrollPos = container.scrollTop;
+		const threshold = height + scrollPos;
+
+		if(threshold >= scrollHeight){
+			setLoadingMore(true);
+			await loadMore();
+			setLoadingMore(false);
+		}
 	};
 
 	return(
-		<div className="search">
+		<div className="search" onScroll={onScroll}>
 
-			<SearchResults playlist={playlist} results={results} />
+			<SearchResults playlist={playlist} results={results} loadingMore={loadingMore} />
 			<div className="search__menu">
 				<Link className="search__button" to="../">
 					<BackArrowIcon className="search__buttonIcon" />
