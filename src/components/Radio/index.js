b579f1b58df75ab81f302137783560ed02ea1da9
Carl Hallén Jansson <callehj@hotmail.se>
2020-03-30 15:02:56 +0200

    Add onChange handler

--- src/components/Radio/index.js
+++ src/components/Radio/index.js
@@ -4,10 +4,16 @@ import PropTypes from "prop-types";
 // Css
 import "./style.css";
 
-const Radio = ({ name, id, value, selected, className = "" }) => {
+const Radio = ({ name, id, value, selected, className = "", onChange = null }) => {
 	return(
 		<span className={`radio ${className}`}>
-			<input id={id} type="radio" defaultChecked={selected === value} name={name} value={value} />
+			<input
+				id={id} type="radio"
+				defaultChecked={selected === value}
+				name={name}
+				value={value}
+				onChange={onChange}
+			/>
 			<span className="custom-radio"></span>
 		</span>
 	);
@@ -18,7 +24,8 @@ Radio.propTypes = {
 	id: PropTypes.string.isRequired,
 	value: PropTypes.string.isRequired,
 	selected: PropTypes.string,
-	className: PropTypes.string
+	className: PropTypes.string,
+	onChange: PropTypes.func
 };
 
 export default Radio;
